c490034311bee8fb413228e80fe27517326150ea
kscardinal <kschang@cardinalhs.net>
2025-10-20 13:12:08 -0400

    Working on signals

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Greeting } from '../components/greeting/greeting';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Greeting],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
-
+  homeMessage = signal('Hello, world!');
 }
